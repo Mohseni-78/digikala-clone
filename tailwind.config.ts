33227852e1bf2f00d51579ddad386f6db7adbb7e
Alireza Mohseni <mohseni.arz@gmail.com>
2025-06-11 00:41:54 +0330

    init: setup project with Next.js 15 and base folder structure

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{ts,tsx}"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#ef4056", // قرمز دیجی‌کالا
+                secondary: "#3f4064", // خاکستری دیجی‌کالا
+            },
+            borderRadius: {
+                xl: "1rem",
+            },
+        },
+    },
+    plugins: [],
+}
